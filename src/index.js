0fda3f8ac48f8f0ee12822c27d7914efd570ae43
qifu <fuqiqi12345@gmail.com>
2018-02-08 10:28:21 -0500

    full function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search'
 const API_KEY = "AIzaSyCWexOCwrnwKeqZfj93lVOSrRJwSaqTK_E"
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail';
-
+import _ from 'lodash'
 
 class App extends Component {
     constructor(props) {
@@ -16,7 +16,10 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
+        this.videoSearch('surfboards')
+    }
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (data) => {
             this.setState({
                 videos: data,
                 selectedVideo: data[0]
@@ -24,9 +27,10 @@ class App extends Component {
         });
     }
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)},300)
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <div>
                     <VideoDetail video={this.state.selectedVideo} />
                     <VideoList
